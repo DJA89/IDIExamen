c97e3e4c93d9cc52fef7f6b374dac8472b257d5c
DJA <drod89@gmail.com>
2019-01-07 14:22:37 +0100

    Ej7

---
+++ Ej7/MyGLWidget.h
@@ -0,0 +1,83 @@
+#define GLM_FORCE_RADIANS
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLWidget>
+#include <QOpenGLShader>
+#include <QOpenGLShaderProgram>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "model.h"
+
+class MyGLWidget : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core
+{
+  Q_OBJECT
+
+  public:
+    MyGLWidget (QWidget *parent=0);
+    ~MyGLWidget ();
+
+  protected:
+    // initializeGL - Aqui incluim les inicialitzacions del contexte grafic.
+    virtual void initializeGL ( );
+    // paintGL - Mètode cridat cada cop que cal refrescar la finestra.
+    // Tot el que es dibuixa es dibuixa aqui.
+    virtual void paintGL ( );
+    // resizeGL - És cridat quan canvia la mida del widget
+    virtual void resizeGL (int width, int height);
+    // keyPressEvent - Es cridat quan es prem una tecla
+    virtual void keyPressEvent (QKeyEvent *event);
+    // mouse{Press/Release/Move}Event - Són cridades quan es realitza l'event
+    // corresponent de ratolí
+    virtual void mousePressEvent (QMouseEvent *event);
+    virtual void mouseReleaseEvent (QMouseEvent *event);
+    virtual void mouseMoveEvent (QMouseEvent *event);
+  public slots:
+    void change_zoom(int);
+  signals:
+    void zoom_changed(int);
+  private:
+    void createBuffersPatricio ();
+    void createBuffersMoo();
+    void createBuffersTerraIParet ();
+    void carregaShaders ();
+    void iniEscena ();
+    void iniCamera ();
+    void projectTransform ();
+    void viewTransform ();
+    void modelTransformTerra ();
+    void modelTransformPatricio1();
+    void modelTransformMoo();
+    void calculaCapsaModel ();
+    void calculaCapsaModelMoo();
+    glm::mat4 TGPatricio();
+    // VAO i VBO names
+    GLuint VAO_Patr;
+    GLuint VAO_Terra;
+    GLuint VAO_Moo;
+    // Program
+    QOpenGLShaderProgram *program;
+    // uniform locations
+    GLuint transLoc, projLoc, viewLoc;
+    // attribute locations
+    GLuint vertexLoc, normalLoc, matambLoc, matdiffLoc, matspecLoc, matshinLoc;
+
+    // model
+    Model patr;
+    Model moo;
+    // paràmetres calculats a partir de la capsa contenidora del model
+    glm::vec3 centrePatr;
+    glm::vec3 centreMoo;
+    float escala, escalaMoo;
+    // radi de l'escena
+    float radiEsc;
+    float ra, FOV, FOV_INI;
+
+    typedef  enum {NONE, ROTATE, ZOOM} InteractiveAction;
+    InteractiveAction DoingInteractive;
+    int xClick, yClick;
+    float angleY, angleX, angleYPatrMoo;
+    bool perspectiva;
+    glm::vec3 vrp;
+};
